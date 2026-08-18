ee0826dc7ba326e79ecdf75c0ef6888523828f8c
AdpKung <oofjmjzx@gmail.com>
2026-08-18 19:17:07 +0700

    Fix: JSX syntax error in CheckDocument.jsx

--- src/pages/CheckDocument.jsx
+++ src/pages/CheckDocument.jsx
@@ -550,6 +550,7 @@ const CheckDocument = () => {
                             </div>
                           )}
                         </>
+                      )}
                     </div>
                     )}
 
